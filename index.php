e623d3fec4d572c0022d74f3a585190ce82e942c
gurkansen <gurkan.sen35@gmail.com>
2017-11-29 18:23:35 +0300

    Init

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+// $kitaplar = array('Harry Potter', 'Yüzüklerin Efendisi', 'Game Of Thrones');
+
+// $kitaplar = [
+//   'Rowling' => 'Harry Potter',
+//   'Tolkien' => ['Yüzüklerin Efendisi', 'Hobbit', 'Silmarillion'],
+//   'Martin' => 'Game Of Thrones'
+// ];
+
+// print_r($kitaplar); // Array'deki değerleri listeler
+// var_dump($kitaplar); // Array'deki değerleri detaylı listeler
+// echo $kitaplar['Tolkien'][0];
+
+
+$soz = 'Yurtta barış dünyada barış!';
+
+// ....
+
+// echo $soz2;
+echo '<a href="">Yurtta</a>';
+
+// echo $soz . '<br>';
+//
+// $kelimeler = explode(' ', $soz); // Array'dekileri Parçalar
+//
+// print_r($kelimeler);
+//
+// echo '<br>';
+//
+// $soz2 = implode('-', $kelimeler); // Array'dekileri birleştirir
+//
+// $soz2 = mb_strtolower($soz2); // Her şeyi küçültür
+
+
+
+// ßecho $soz2;
+
+// echo 'hello paü';
+
+// $rakam = 5; // int
+// $sayfa = '5'; // string
+
+// echo 5 * 4;
+
+// $isim = 'Gürkan';
+
+// echo 'Selam ' . $isim; // Nokta stringleri birleştirir
+
+// if (isset($rakam)) { // Değişken tanımlı diye kontrol eder
+//   echo 'evet';
+// }
+
+// unset($rakam); // değişkeni tanımsız yapar (siler)
+
+// header("Location: $sayfa"); // Yönlendirme için
+
+
+
+
+?>
