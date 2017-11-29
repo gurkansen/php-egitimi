051755a5ea13681faa08a84f3dbbbc7844cc9837
gurkansen <gurkan.sen35@gmail.com>
2017-11-29 19:06:55 +0300

    🔧  Change index.php

--- index.php
+++ index.php
@@ -30,6 +30,8 @@ $kelimeler = explode(' ', $soz);
 // echo str_replace('dünyada', 'marsta', $soz);
 
 echo str_replace('ı', 'i', $soz);
+echo str_replace('ü', 'u', $soz);
+echo str_replace('ş', 's', $soz);
 
 // echo '<a href="">Yurtta</a>';
 
